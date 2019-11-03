8b06f2ffcd3960effc1d794d96224e3b7b5a3a78
yurie <yamayuri@gmail.com>
2019-11-03 19:17:42 +0900

    added support for C++ (#2)

--- include/mruby_tlsf.h
+++ include/mruby_tlsf.h
@@ -5,6 +5,8 @@
 #ifndef MRUBY_TLSF_H
 #define MRUBY_TLSF_H
 
+MRB_BEGIN_DECL
+
 MRB_API mrb_state* mrb_open_tlsf(void *mem, size_t bytes);
 MRB_API void* mrb_tlsf_allocf(mrb_state *mrb, void *p, size_t size, void *ud);
 MRB_API struct mrb_tlsf_t* mrb_tlsf_init(void *mem, size_t bytes);
@@ -13,6 +15,8 @@ MRB_API void mrb_close_tlsf(mrb_state *mrb);
 MRB_API mrb_value mrb_tlsf_total_memory(mrb_state *mrb, mrb_value obj);
 MRB_API mrb_value mrb_tlsf_used_memory(mrb_state *mrb, mrb_value obj);
 MRB_API mrb_value mrb_tlsf_available_memory(mrb_state *mrb, mrb_value obj);
+
+MRB_END_DECL
 
 #endif  /* MRUBY_TLSF_H */
 
